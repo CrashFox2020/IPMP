7edb358ff2595dd8bc16eed01e7deccd80ba1527
CrashFox2020 <105661466+CrashFox2020@users.noreply.github.com>
2023-02-10 19:38:22 +0530

    Add files via upload

--- Arrays/max_1s.cpp
+++ Arrays/max_1s.cpp
@@ -2,19 +2,30 @@
 using namespace std;
 
 int main(){
-    int arr[4][4]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+    int arr[4][4]={ {0, 0, 0, 1},
+                    {0, 1, 1, 1},
+                    {1, 1, 1, 1},
+                    {0, 0, 0, 0}};
     int max=0;
     int index=0;
     for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            if(arr[i][j]==1){
-                if(max<4-j){
-                    max=4-j;
-                    index=i;
-                    if(max==4){
-                        break;
-                    }
-                }
+        if(arr[0][i]==1){
+            max=4-i;
+            break;
+        }
+    }
+    for(int i=1;i<4;i++){
+        int j=4-max-1;
+        if(arr[i][j]==1){
+            while(j>=0&&arr[i][j]==1){
+                j--;
+            }
+            if(4-j-1>max){
+                index=i;
+                max=4-j-1;
+            }
+            if(max==4){
+                break;
             }
         }
     }
